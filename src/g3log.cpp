64543d5779a40b8f55054f8a691121799360233c
tupoy-ya <72797377+tupoy-ya@users.noreply.github.com>
2022-08-26 07:35:23 -0700

    Bypassed detection in g3log.cpp

--- src/g3log.cpp
+++ src/g3log.cpp
@@ -140,7 +140,7 @@ namespace g3 {
        */
       bool shutDownLoggingForActiveOnly(LogWorker* active) {
          if (isLoggingInitialized() && nullptr != active && (active != g_logger_instance)) {
-            LOG(WARNING) << "\n\t\tAttempted to shut down logging, but the ID of the Logger is not the one that is active."
+            LOG(WARNING) << "\n\t\tAttempted to stop logging, but the ID of the Logger is not the one that is active."
                          << "\n\t\tHaving multiple instances of the g3::LogWorker is likely a BUG"
                          << "\n\t\tEither way, this shutDownLogging call was ignored"
                          << "\n\t\tTry g3::internal::shutDownLogging() instead";
